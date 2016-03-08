79383ec0bad1e433aaa18007465b4a091b229ac8
Peter Fousteris <lostpfg90@gmail.com>
2016-03-08 12:38:38 +0200

    Several code Updates
    -- New more compact version of code
    -- Removed unnecessary functions and code lines.

--- sha256.m
+++ sha256.m
@@ -34,18 +34,12 @@ function out = sha256( msg )
     % Split padded message to N (512-bit) blocks.(§6)
     [M,total_blocks] = split2block( padded_msg,padded_len );
     W = zeros( 64, 32 );
+    H = zeros( 8, 32 );
     % Main SHA-256 compuation process.(§6.2.2)
+    for j = 1:8 % Load initial hash values at first iteration.
+        H(j,:) = hexToBinaryVector( default_hash( j , : ) , 32 );
+    end
     for i = 1:total_blocks % For every block M(i).
-        if i == 1 % Load initial hash values at first iteration.
-            h0 = hexToBinaryVector( default_hash( 1 , : ) , 32 );
-            h1 = hexToBinaryVector( default_hash( 2 , : ) , 32 );
-            h2 = hexToBinaryVector( default_hash( 3 , : ) , 32 ); 
-            h3 = hexToBinaryVector( default_hash( 4 , : ) , 32 );
-            h4 = hexToBinaryVector( default_hash( 5 , : ) , 32 );
-            h5 = hexToBinaryVector( default_hash( 6 , : ) , 32 );
-            h6 = hexToBinaryVector( default_hash( 7 , : ) , 32 );
-            h7 = hexToBinaryVector( default_hash( 8 , : ) , 32 );
-        end
         % Step 1 - Prepare the message schedule.
         for j = 1:64
             if j >= 1 && j <= 16
@@ -56,14 +50,14 @@ function out = sha256( msg )
         end
         % Step 2 - Initialize the eight working variables, a, b, c, d, e, f, g,
         % and h, with the (i-1)st hash value.
-        a = h0;
-        b = h1;
-        c = h2;
-        d = h3;
-        e = h4;
-        f = h5;
-        g = h6;
-        h = h7;
+        a = H(1,:);
+        b = H(2,:);
+        c = H(3,:);
+        d = H(4,:);
+        e = H(5,:);
+        f = H(6,:);
+        g = H(7,:);
+        h = H(8,:);
         % For t=0 to 63.
         for t = 1:64
             T1 = mod32add( h, capSigma1(e),ch( e, f, g ),hexToBinaryVector( K( t,: ) , 32 ), W( t,:  ) );
@@ -78,18 +72,19 @@ function out = sha256( msg )
             a = mod32add( T1, T2 );
         end
         % Step 4 - Compute the ith intermediate hash value H(i).
-        h0 =  mod32add( a, h0 );
-        h1 =  mod32add( b, h1 );
-        h2 =  mod32add( c, h2 );
-        h3 =  mod32add( d, h3 );
-        h4 =  mod32add( e, h4 );
-        h5 =  mod32add( f, h5 );
-        h6 =  mod32add( g, h6 );
-        h7 =  mod32add( h, h7 );    
+        H(1,:) =  mod32add( a, H(1,:)  );
+        H(2,:) =  mod32add( b, H(2,:)  );
+        H(3,:) =  mod32add( c, H(3,:)  );
+        H(4,:) =  mod32add( d, H(4,:)  );
+        H(5,:) =  mod32add( e, H(5,:)  );
+        H(6,:) =  mod32add( f, H(6,:)  );
+        H(7,:) =  mod32add( g, H(7,:)  );
+        H(8,:) =  mod32add( h, H(8,:)  );    
+
     end
     % Final Step - After hash process the resulting 256-bit message digest
     % of the message, M, is:
-    out = calcout( h0, h1, h2, h3, h4, h5 ,h6, h7 );
+    out = binaryVectorToHex( horzcat( H(1,:), H(2,:), H(3,:), H(4,:), H(5,:), H(6,:) ,H(7,:), H(8,:)  ) );
 end
 
 function [out,len] = padder( msg )
@@ -127,29 +122,26 @@ end
 
 function out = mod32add( varargin )
     % Function mod32add : Performs addition modulo 32.(§3.2.1)
-    narginchk(1, inf)
     out = 0; % initialise return arguments
-    nums = zeros( 1, length(varargin) ); % Set array of input variables.
-    for i=1:length(varargin) % Calculate addition
-        nums(i) = fix2mod(varargin{i});
-        out = out + nums(i);
+    for i = 1:length( varargin ) % Calculate addition
+        out = out + fix2mod(varargin{i});
     end
     % Perform modulo 32 operation.
-    out1 = dec2bin( mod(( out ),2^32),32 ) ;
-    out = logical( out1(:)'-'0' ); % Cast output to logical array.
+    out = dec2bin( mod( ( out ), 2^32 ),32 ) ;
+    out = logical( out(:)'-'0' ); % Cast output to logical array.
 end
 
 function out = rotr( word, pos )
     % Function rotr : Performs ROTR (Cirular right shift) operation.(§3.2.1)
     out = zeros( 1, length( word ) );
-    out( pos + 1 : end ) = word( 1 : end - pos + 0 ); 
-    out( 1 : pos ) = word( end - pos + 1 : end ); 
+    out( pos + 1:end ) = word( 1:end - pos + 0 ); 
+    out( 1:pos ) = word( end - pos + 1 : end ); 
 end
 
 function out = shr( word, pos )
     % Function rotr : Performs SHR (Right shift) operation.(§3.2.1)
     out = zeros( 1, length( word ) );
-    out( 1 + pos : end ) = word( 1 : end - pos ); 
+    out( 1 + pos:end ) = word( 1:end - pos ); 
 end
 
 function out = maj( x, y, z )
@@ -179,19 +171,5 @@ end
 
 function out = sigma1( word )
     % Function ó1 : Performs Ó0 operation.(§4.1.2)
-    out = bitxor( rotr( word, 17 ), rotr( word, 19 ) );
-    out = bitxor( out, shr( word, 10 ) );
-end
-
-function out = calcout( h0, h1, h2, h3, h4, h5 ,h6, h7 )
-    % Function calcout : Merges the output string (256-bit).
-    t1 = binaryVectorToHex(h0);
-    t2 = binaryVectorToHex(h1);
-    t3 = binaryVectorToHex(h2);
-    t4 = binaryVectorToHex(h3);
-    t5 = binaryVectorToHex(h4);
-    t6 = binaryVectorToHex(h5);
-    t7 = binaryVectorToHex(h6);
-    t8 = binaryVectorToHex(h7);
-    out = strcat( t1, t2, t3, t4, t5, t6, t7, t8 );
+    out = bitxor( bitxor( rotr( word, 17 ), rotr( word, 19 ) ), shr( word, 10 ) );
 end
